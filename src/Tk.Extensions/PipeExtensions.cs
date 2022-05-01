f979738e8cd629f821f95ba461fa2a1d34289b72
tonycknight <tonycknight@users.noreply.github.com>
2022-05-01 13:35:29 +0100

    remove redundant code

--- src/Tk.Extensions/PipeExtensions.cs
+++ src/Tk.Extensions/PipeExtensions.cs
@@ -11,19 +11,5 @@ namespace Tk.Extensions
 
             return selector(value);
         }
-
-
-        [DebuggerStepThrough]
-#pragma warning disable CS8601 // Possible null reference assignment.
-        public static TResult PipeIfNotNull<TValue, TResult>(this TValue value, Func<TValue, TResult> selector, TResult defaultValue = default)
-#pragma warning restore CS8601 // Possible null reference assignment.
-                where TValue : class
-        {
-            selector.ArgNotNull(nameof(selector));
-
-            return value != null
-                ? selector(value)
-                : defaultValue;
-        }
     }
 }
