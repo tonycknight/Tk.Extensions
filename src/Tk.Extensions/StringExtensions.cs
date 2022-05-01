63602e0ea9f1bee9f4a45801f515c529e4efbfe6
tonycknight <tonycknight@users.noreply.github.com>
2022-05-01 01:00:35 +0100

    and more...

--- src/Tk.Extensions/StringExtensions.cs
+++ src/Tk.Extensions/StringExtensions.cs
@@ -5,8 +5,10 @@ namespace Tk.Extensions
     public static class StringExtensions
     {
         [DebuggerStepThrough]
-        public static string Format(this string value, string format) => string.Format(format, value);
+        public static string Join(this IEnumerable<string> values, string delimiter) => string.Join(delimiter, values);
 
+        [DebuggerStepThrough]
+        public static string Format(this string value, string format) => string.Format(format, value);
 
         [DebuggerStepThrough]
         public static string TrimEnd(this string value, string trailing)
---
+++ test/Tk.Extensions.Tests/StringExtensionsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Tk.Extensions.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void Join_Null_ThrowsException()
+        {
+            string[] xs = null;
+
+            Action a = () => xs.Join(",");
+
+            a.Should().Throw<ArgumentNullException>().WithMessage("?*");
+        }
+
+        [Fact]
+        public void Join_Empty_ReturnsEmpty()
+        {
+            var xs = new string[0];
+
+            var r = xs.Join(" ");
+
+            r.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(7)]
+        public void Join_NonEmpty_ReturnsNonEmpty(int count)
+        {
+            var xs = Enumerable.Range(1, count);
+
+            var cs = xs.Select(i => i.ToString()[0]).ToArray();
+            var ss = xs.Select(i => i.ToString());
+
+            var r = ss.Join("");
+
+            var expected = new String(cs);
+            
+            r.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(7)]
+        public void Join_NonEmptyDelimiter_ReturnsNonEmpty(int count)
+        {
+            var d = 'd';
+            var xs = Enumerable.Range(1, count);
+
+            var ss = xs.Select(i => "");
+
+            var r = ss.Join(d.ToString());
+
+            var expected = new String(d, count - 1);
+            
+            r.Should().Be(expected);
+        }
+    }
+}
