b54e04bfdcf1670582fdc4d5255b25974d70d4a8
tonycknight <tonycknight@users.noreply.github.com>
2022-05-01 14:15:53 +0100

    TimeProvider tests

---
+++ test/Tk.Extensions.Tests/TimeProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Tk.Extensions.Tests
+{
+    public class TimeProviderTests
+    {
+        [Fact]
+        public void UtcNow_ReturnsUtcTimeKind()
+        {
+            var tp = new TimeProvider();
+
+            var r = tp.UtcNow();
+
+            r.Kind.Should().Be(DateTimeKind.Utc);
+        }
+
+        [Fact]
+        public void UtcNow_ReturnsUtcTime()
+        {
+            var now = DateTime.UtcNow;
+            var tp = new TimeProvider();
+
+            var r = tp.UtcNow();
+                        
+            r.Should().BeCloseTo(now, TimeSpan.FromSeconds(1));
+        }
+
+    }
+}
