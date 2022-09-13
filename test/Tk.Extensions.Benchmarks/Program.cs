c3c4accb930b2fd449489491f8aab71fd1462341
tonycknight <tonycknight@users.noreply.github.com>
2022-09-14 00:27:53 +0100

    benchmark project shell

---
+++ test/Tk.Extensions.Benchmarks/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Tk.Extensions.Benchmarks 
+{
+    public class Program
+    {
+        public int Main(string[] args)
+        {
+            try
+            {
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ResetColor();
+                return 1;
+            }
+        }
+    }
+}
